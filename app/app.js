6ef1c777b72cb465aaaeea01985c80d2b2818331
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-04-29 03:34:32 -0500

    Crear vistas de app

--- app/app.js
+++ app/app.js
@@ -1,39 +1,43 @@
-'use strict';
-
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
-  'ngActionCable'
- ])
- .config(['$locationProvider', '$routeProvider',
-  function ($locationProvider, $routeProvider) {
-   $locationProvider.hashPrefix('!');
+  'ngActionCable',
+])
+  .config(['$locationProvider', '$routeProvider',
+    function($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
 
-   $routeProvider.otherwise({redirectTo: '/view1'});
+      $routeProvider
+        .when('/', {
+          templateUrl: 'dashboard/show.html',
+        })
+        .when('/profile', {
+          templateUrl: 'profile/show.html',
+        })
+        .otherwise({redirectTo: '/'});
+    }])
+  .controller('dashboardController', function($scope) {
+  })
+  .run(function($rootScope, ActionCableChannel) {
+    $rootScope.inputText = '';
+    $rootScope.myData = [];
 
-  }])
- .run(function ($rootScope, ActionCableChannel) {
-
-  $rootScope.inputText = "";
-  $rootScope.myData = [];
-
-  // connect to ActionCable
-  var consumer = new ActionCableChannel("FeedChannel", {user_id: 1});
-  var callback = function (message) {
-   debugger;
-   $rootScope.myData.push(message);
-  };
-  consumer.subscribe(callback).then(function () {
-   $rootScope.sendToMyChannel = function (message) {
-    consumer.send(message, 'send_a_message');
-   };
-   $rootScope.$on("$destroy", function () {
-    consumer.unsubscribe().then(function () {
-     $rootScope.sendToMyChannel = undefined;
+    // connect to ActionCable
+    const consumer = new ActionCableChannel('FeedChannel', {user_id: 1});
+    const callback = function(message) {
+      $rootScope.myData.push(message);
+    };
+    consumer.subscribe(callback).then(function() {
+      $rootScope.sendToMyChannel = function(message) {
+        consumer.send(message, 'send_a_message');
+      };
+      $rootScope.$on('$destroy', function() {
+        consumer.unsubscribe().then(function() {
+          $rootScope.sendToMyChannel = undefined;
+        });
+      });
     });
-   });
   });
- });
